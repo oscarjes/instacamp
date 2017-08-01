7b35148388a42bbabdfd2817d048f49da96a75c6
“Stanley <dannyshien@gmail.com>
2017-08-01 15:19:59 +0700

    set up db seeds and generate fake data with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Seeding data"
+if Photo.count > 0 
+  print "WARNING: there is existing data. Do you want to delete all and seed again? (y/n) "
+  answer = STDIN.gets.chomp
+  if answer == "y" || answer == ""
+    puts "Deleting photos. New ones will be seeded"
+    Photo.delete_all
+  else
+    puts "Your data is safe. Goodbye."
+    return
+  end
+end
+
+Photo.create!(url: "http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,fl_lossy,h_1440,w_720,f_auto,q_auto/v1/158015/loi_tran_mb4e76.jpg", username: "Loi", likes_count: 7, caption: "Looking good, sir!") 
+Photo.create!(url: "http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,f_auto,h_3000,q_90,w_1200/v1/158015/55957aa69ca847bf8474a1b5c0b4f7e6_hcev12.png", username: "Harley", likes_count: 5002, caption: "Act quick, he's getting married soon!")
+Photo.create!(url: "http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,f_auto,h_3000,q_90,w_1200/Luvocracy_327_1_x8yw9e.jpg", username: "Charles", likes_count: 4094, caption: "Your Korean badboy with a poodle...")
+
+10.times do
+  username = Faker::HowIMetYourMother.character
+  caption = Faker::HowIMetYourMother.quote
+  url = Faker::LoremPixel.image("500x500", false, 'sports', nil, username)
+  Photo.create!(url: url, username: username,
+                caption: caption, likes_count: Faker::Number.number(5))  
+end
+
+puts "Seeded #{Photo.count} photos."
+
+
+
