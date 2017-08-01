0afe40f58156ffe8881815e1eb25072296d7d356
“Stanley <dannyshien@gmail.com>
2017-08-01 15:51:55 +0700

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,21 @@
 puts "Seeding data"
+if Photo.count > 0
+  puts "WARNING: there is existing data."
+  puts "To re-seed, first go to rails console and type 'Photo.delete_all'"
+  puts "For heroku, type 'heroku run rails console'"
+  exit
+end
+
+  # print "WARNING: there is existing data. Do you want to delete all and seed again? (y/n) "
+  # answer = STDIN.gets.chomp
+  # if answer == "y" || answer == ""
+  #   puts "Deleting photos. New ones will be seeded"
+  #   Photo.delete_all
+  # else
+  #   puts "Your data is safe. Goodbye."
+  #   return0
+  # end
+
 
 Photo.create!(url: "http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,fl_lossy,h_1440,w_720,f_auto,q_auto/v1/158015/loi_tran_mb4e76.jpg", username: "Loi", likes_count: 7, caption: "Looking good, sir!") 
 Photo.create!(url: "http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,f_auto,h_3000,q_90,w_1200/v1/158015/55957aa69ca847bf8474a1b5c0b4f7e6_hcev12.png", username: "Harley", likes_count: 5002, caption: "Act quick, he's getting married soon!")
