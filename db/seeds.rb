a6cc433bfd8d5a961e5e8059f770604afcc87429
“Stanley <dannyshien@gmail.com>
2017-08-01 15:43:17 +0700

    delete if else logic from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,4 @@
 puts "Seeding data"
-if Photo.count > 0 
-  print "WARNING: there is existing data. Do you want to delete all and seed again? (y/n) "
-  answer = STDIN.gets.chomp
-  if answer == "y" || answer == ""
-    puts "Deleting photos. New ones will be seeded"
-    Photo.delete_all
-  else
-    puts "Your data is safe. Goodbye."
-    return
-  end
-  
-end
 
 Photo.create!(url: "http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,fl_lossy,h_1440,w_720,f_auto,q_auto/v1/158015/loi_tran_mb4e76.jpg", username: "Loi", likes_count: 7, caption: "Looking good, sir!") 
 Photo.create!(url: "http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,f_auto,h_3000,q_90,w_1200/v1/158015/55957aa69ca847bf8474a1b5c0b4f7e6_hcev12.png", username: "Harley", likes_count: 5002, caption: "Act quick, he's getting married soon!")
