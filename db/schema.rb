072924c14565e65edb3786d3847d0ae3d4983524
oscarjes <oscar@jesionek.co>
2017-08-02 11:56:37 +0700

    Add photo submit function and avatar pic + description of when pic is posted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801065557) do
+ActiveRecord::Schema.define(version: 20170802033745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20170801065557) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_url"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,16 +17,13 @@ end
   # end
 
 
-Photo.create!(url: "http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,fl_lossy,h_1440,w_720,f_auto,q_auto/v1/158015/loi_tran_mb4e76.jpg", username: "Loi", likes_count: 7, caption: "Looking good, sir!") 
-Photo.create!(url: "http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,f_auto,h_3000,q_90,w_1200/v1/158015/55957aa69ca847bf8474a1b5c0b4f7e6_hcev12.png", username: "Harley", likes_count: 5002, caption: "Act quick, he's getting married soon!")
-Photo.create!(url: "http://res.cloudinary.com/hrscywv4p/image/upload/c_limit,f_auto,h_3000,q_90,w_1200/Luvocracy_327_1_x8yw9e.jpg", username: "Charles", likes_count: 4094, caption: "Your Korean badboy with a poodle...")
-
-10.times do
+20.times do
   username = Faker::HowIMetYourMother.character
   caption = Faker::HowIMetYourMother.quote
   url = Faker::LoremPixel.image("500x500", false, 'sports', nil, username)
+  avatar_url = Faker::LoremPixel.image("50x50", false, 'sports', nil, username)
   Photo.create!(url: url, username: username,
-                caption: caption, likes_count: Faker::Number.number(5))  
+                caption: caption, likes_count: Faker::Number.number(5), avatar_url: avatar_url)  
 end
 
 puts "Seeded #{Photo.count} photos."
